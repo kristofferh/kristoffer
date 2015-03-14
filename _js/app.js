d78f6c4bb9aaa8e27e1dd8c088e638f3ecdd7a25
Kris Hedstrom <kris@tumblr.com>
2015-03-14 18:23:44 -0400

    basics

---
+++ _js/app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function hello() {
+    console.log('hi');
+}
+
+hello();
---
+++ _js/test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var hey = function() {
+    console.log('this is a thing, yes.');
+};
+
+module.exports = hey;
