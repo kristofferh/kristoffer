b11341be2a02c641d9d1d98c7cd5a9c8b808c800
Kristoffer Hedstrom <kris.hedstrom@gmail.com>
2016-04-28 14:27:20 -0400

    peekaboo footer

--- pages/_template.jsx
+++ pages/_template.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navigation from 'components/navigation';
+import {config} from 'config';
 
 module.exports = React.createClass({
   propTypes () {
@@ -14,8 +15,10 @@ module.exports = React.createClass({
         <section className='content-wrapper'>
           {this.props.children}
         </section>
-        <footer>
-          {'hi'}
+        <footer className='footer-wrapper'>
+          <div className='footer-inner content-container'>
+            {`${config.name}`}{' — '} <a href={`mailto:${config.email}`}>{config.email}</a>
+          </div>
         </footer>
       </div>
     );
