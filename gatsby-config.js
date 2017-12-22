2cf2c4242b640a2f7db52dc09d6541ff75153200
Kris Hedstrom <kris@nomadhealth.com>
2017-12-22 12:22:59 -0500

    Add config and move html.js

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteMetadata: {
+    siteTitle: "Kristoffer Hedstrom",
+    url: "https://k-create.com",
+    name: "Kristoffer Hedstrom",
+    email: "hello@k-create.com",
+    segment: "KrpuBOMB62nTKrcoFBpMoZQEKKIJllxU",
+    description:
+      "Website for Kristoffer Hedstrom: Interactive designer and developer from Brooklyn, NY",
+    twitter: "@_kris",
+    shareImage: "https://k-create.com/images/shaky.gif"
+  }
+};
