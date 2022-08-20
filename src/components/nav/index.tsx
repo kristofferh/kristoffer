00de3e3d7b3d744b6405919bae523de595041479
Kristoffer Hedstrom <kris.hedstrom@gmail.com>
2022-08-20 18:08:45 +0200

    building out more of the nav

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,19 +1,42 @@
 import React, { useState } from "react";
-import { Controls } from "./styles";
+import {
+  Controls,
+  NavContainer,
+  MainNavContainer,
+  MainNav,
+  UtilityNavContainer,
+  UtilityNav,
+} from "./styles";
 import { IconButton } from "../icon-button";
 import { Burger } from "../burger";
 import { Panel } from "../panel";
 import { motion, AnimatePresence } from "framer-motion";
 
+const mainNavVariants = {
+  enter: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.05,
+      delayChildren: 0.15,
+      duration: 0.15,
+    },
+  },
+  exit: {
+    opacity: 0,
+    transition: {
+      staggerChildren: 0.01,
+      staggerDirection: -1,
+    },
+  },
+};
+
 export const Nav: React.FC = () => {
   const [open, setOpen] = useState(false);
   const [showNav, setShowNav] = useState(false);
   const handleButtonClick = () => {
-    console.log("click");
     setOpen(!open);
   };
   const handleAnimationEnd = () => {
-    console.log("done");
     setShowNav(open);
   };
   return (
@@ -32,33 +55,79 @@ export const Nav: React.FC = () => {
         onAnimationEnd={handleAnimationEnd}
       >
         <AnimatePresence>
-          {showNav ? (
-            <nav>
-              <ul>
-                <motion.li
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  exit={{ opacity: 0 }}
-                >
-                  Work
-                </motion.li>
-                <motion.li
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  exit={{ opacity: 0 }}
-                >
-                  About
-                </motion.li>
-                <motion.li
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  exit={{ opacity: 0 }}
-                >
-                  Resume
-                </motion.li>
-              </ul>
-            </nav>
-          ) : null}
+          <NavContainer>
+            <div
+              style={{
+                background: "red",
+                width: 80,
+                height: 80,
+                flexShrink: 0,
+              }}
+            >
+              Logo
+            </div>
+            {showNav ? (
+              <>
+                <MainNavContainer>
+                  <MainNav>
+                    <motion.li
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      exit={{ opacity: 0 }}
+                    >
+                      About
+                    </motion.li>
+                    <motion.li
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      exit={{ opacity: 0 }}
+                    >
+                      Work
+                    </motion.li>
+                    <motion.li
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      exit={{ opacity: 0 }}
+                    >
+                      Resume
+                    </motion.li>
+                  </MainNav>
+                </MainNavContainer>
+                <UtilityNavContainer>
+                  <UtilityNav>
+                    <motion.li
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      exit={{ opacity: 0 }}
+                    >
+                      Codepen
+                    </motion.li>
+                    <motion.li
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      exit={{ opacity: 0 }}
+                    >
+                      Github
+                    </motion.li>
+                    <motion.li
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      exit={{ opacity: 0 }}
+                    >
+                      Tumblr
+                    </motion.li>
+                    <motion.li
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      exit={{ opacity: 0 }}
+                    >
+                      Twitter
+                    </motion.li>
+                  </UtilityNav>
+                </UtilityNavContainer>
+              </>
+            ) : null}
+          </NavContainer>
         </AnimatePresence>
       </Panel>
     </>
--- src/components/nav/styles/index.tsx
+++ src/components/nav/styles/index.tsx
@@ -5,12 +5,12 @@ import { motion } from "framer-motion";
 export const enter = keyframes`
   from { 
     opacity: 0;
-    transform: translate3d(-50%, 100%, 0);
+    transform: translate3d(0, 100%, 0);
   }
 
   to { 
     opacity: 1;
-    transform: translate3d(-50%, 0, 0);
+    transform: translate3d(0, 0, 0);
   }
 `;
 
@@ -35,4 +35,36 @@ export const Controls = styled.nav<{ wide?: boolean }>`
   z-index: 100;
   overflow: hidden;
   animation: ${enter} 0.25s 0.35s ease-in forwards;
+`;
+
+export const NavContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  overflow: auto;
+`;
+
+export const MainNavContainer = styled(motion.nav)`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  margin: 2rem 0;
+`;
+
+export const UtilityNavContainer = styled(motion.nav)`
+  margin-bottom: 8rem;
+`;
+
+export const MainNav = styled(motion.ul)`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`;
+
+export const UtilityNav = styled(motion.ul)`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  gap: 0 1rem;
 `;
